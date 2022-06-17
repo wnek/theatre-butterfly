3af680c68fb64332691a99c791be656516e0d9c9
Mariusz <wnek.mariusz@gmail.com>
2022-06-17 15:09:27 +0200

    Lights, shadows

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Canvas, useFrame, useThree } from "@react-three/fiber";
-import { useGLTF, OrbitControls } from "@react-three/drei";
+import { useGLTF, OrbitControls, Sparkles, Environment } from "@react-three/drei";
 import { EffectComposer, DepthOfField, Bloom, Vignette} from "@react-three/postprocessing"
-import {KernelSize, BlendFunction} from "postprocessing"
+import { KernelSize, BlendFunction} from "postprocessing"
 import { useRef } from "react";
 import { proxy, useSnapshot } from 'valtio'
 
@@ -28,28 +28,30 @@ function Butterfly({...props}){
   const { nodes, materials } = useGLTF('/butterfly-cleaned.glb')
   return(
     <group ref={group} {...props} dispose={null}>
-      <mesh geometry={nodes.tree.geometry} material={materials.leaf}  receiveShadow />
-      <mesh geometry={nodes.rwing.geometry} material={materials.wing} castShadow />
-      <mesh geometry={nodes.body.geometry} material={materials.wing} castShadow  />
-      <mesh geometry={nodes.tree_tree003.geometry} material={materials['bark.001']} receiveShadow/>
-      <mesh geometry={nodes.tree_tree003_1.geometry} material={materials.leaf} receiveShadow/>
-      <mesh geometry={nodes.light.geometry} material={materials.Material} receiveShadow/>
-    </group>
+    <mesh geometry={nodes.tree2.geometry} material={materials.leaf} castShadow receiveShadow />
+    <mesh geometry={nodes.rwing.geometry} material={materials.wing} castShadow>
+      <mesh geometry={nodes.body.geometry} material={materials['Material.001']} castShadow/>
+    </mesh>
+    <mesh geometry={nodes.tree1.geometry} material={materials.leaf} castShadow receiveShadow />
+  </group>
   )
 }
+
 export default function App() {
   return (
   <Canvas gl={{alpha: false}} camera={{near: 0.01, far: 100}} shadows>
-    <color attach="background" args={["#000000"]} />
-    <fog attach="fog" color="black" near={1} far={15} />
-    <ambientLight intensity={40} color={"#002238"}/>
-    <spotLight position={[-4, 60, 20]} intensity={4} color={"#D5BC76"} penumbra={1} castShadow/>
+    <Environment files="/background.hdr" background={"true"}/>
     <Butterfly />
+
+    {/* <color attach="background" args={["#020717"]} /> */}
+    <fog attach="fog" color="#020717" near={1} far={20} />
+    <ambientLight intensity={0.4} color={"#ffffff"}/>
+    <spotLight intensity={2} angle={0.1} position={[20, 30, 30]} penumbra={1} castShadow decay={2} power={20} color={"#D5BC76"} />
+    <Sparkles count={200} scale={10} size={2} speed={0.4} opacity={0.01} />
     <Controls />
     <EffectComposer>
-      <DepthOfField target={[0, 0, 0.9]} focalLength={ 0.01 } bokehScale={1.4}/>
-      <Bloom kernelSize={5} luminanceThreshold={0.45} luminanceSmoothing={1} intensity={1} />
-      <Bloom kernelSize={1} luminanceThreshold={0.4} luminanceSmoothing={1} intensity={2} />
+      <DepthOfField target={[0, 0, 1]} focalLength={ 0.1 } bokehScale={3}/>
+      <Bloom kernelSize={4} luminanceThreshold={0.25} luminanceSmoothing={7} intensity={0.5} />
       <Vignette
         offset={0.5}
         darkness={0.6}
@@ -60,6 +62,7 @@ export default function App() {
         eskil={false}
         blendFunction={BlendFunction.NORMAL}
       />
+    
    
     </EffectComposer>
   </Canvas>
