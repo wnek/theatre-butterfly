261f6a7391ff587ee5a35e137813e629bb5e5277
Mariusz Wnek <mariusz@theatrejs.com>
2022-06-27 17:14:10 +0200

    Add Theatre

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { getProject } from '@theatre/core';
 import { Canvas, useFrame, useThree } from '@react-three/fiber';
 import {
   useGLTF,
@@ -15,6 +16,7 @@ import { KernelSize, BlendFunction } from 'postprocessing';
 import { useRef, useMemo } from 'react';
 import { proxy, useSnapshot } from 'valtio';
 import InstancedModel from '/src/Components/3dmodel';
+import { SheetProvider } from '@theatre/r3f';
 
 const modes = ['translate', 'rotate', 'scale'];
 const state = proxy({ current: null, mode: 0 });
@@ -41,35 +43,41 @@ function Controls() {
 
 export default function App() {
   return (
-    <Canvas gl={{ alpha: false }} camera={{ near: 0.01, far: 1000 }} shadows>
-      <Environment files="/background.hdr" background={'true'} />
+    <Canvas
+      gl={{ alpha: false, preserveDrawingBuffer: true }}
+      camera={{ near: 0.01, far: 1000 }}
+      shadows
+    >
+      <SheetProvider sheet={getProject('Butterfly').sheet('Scene')}>
+        <Environment files="/background.hdr" background={'true'} />
 
-      {/* <color attach="background" args={["#020717"]} /> */}
-      <fog attach="fog" color="#020717" near={1} far={20} />
-      <ambientLight intensity={0.4} color={'#ffffff'} />
-      <spotLight
-        intensity={2}
-        angle={0.1}
-        position={[20, 30, 30]}
-        penumbra={1}
-        castShadow
-        decay={2}
-        power={15}
-        color={'#D5BC76'}
-      />
-      <Sparkles count={200} scale={10} size={2} speed={0.4} opacity={0.01} />
-      <InstancedModel />
-      <Controls />
-      <EffectComposer>
-        <DepthOfField target={[0, 0, 1]} focalLength={0.2} bokehScale={3} />
-        <Bloom
-          kernelSize={4}
-          luminanceThreshold={0.25}
-          luminanceSmoothing={7}
-          intensity={1}
+        <fog attach="fog" color="#020717" near={1} far={20} />
+        <ambientLight intensity={0.4} color={'#ffffff'} />
+        <spotLight
+          uniqueName="butterfly"
+          intensity={2}
+          angle={0.1}
+          position={[20, 30, 30]}
+          penumbra={1}
+          castShadow
+          decay={2}
+          power={15}
+          color={'#D5BC76'}
         />
-        <Vignette eskil={false} blendFunction={BlendFunction.NORMAL} />
-      </EffectComposer>
+        <Sparkles count={200} scale={10} size={2} speed={0.4} opacity={0.01} />
+        <InstancedModel />
+        <Controls />
+        <EffectComposer>
+          <DepthOfField target={[0, 0, 1]} focalLength={0.2} bokehScale={3} />
+          <Bloom
+            kernelSize={4}
+            luminanceThreshold={0.25}
+            luminanceSmoothing={7}
+            intensity={1}
+          />
+          <Vignette eskil={false} blendFunction={BlendFunction.NORMAL} />
+        </EffectComposer>
+      </SheetProvider>
     </Canvas>
   );
 }
