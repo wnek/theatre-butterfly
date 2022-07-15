d9f2b11256e9ad3422ece85aa08cf476e6ba92df
Mariusz_Wnek <mariusz@activetheory.net>
2022-07-15 10:52:00 +0200

    Adjust colors

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import {
   DepthOfField,
   Bloom,
   Vignette,
+  Noise,
 } from '@react-three/postprocessing';
 import { KernelSize, BlendFunction } from 'postprocessing';
 import { proxy, useSnapshot } from 'valtio';
@@ -60,32 +61,35 @@ export default function App() {
       shadows
     >
       <SheetProvider sheet={sheet}>
-        <Environment files="/background.hdr" background={'true'} />
+        <Environment files="/background2.hdr" background={'true'} />
         <fog attach="fog" color="#020717" near={1} far={40} />
-        <ambientLight intensity={0.4} color={'#ffffff'} />
+        <ambientLight intensity={0.4} color={'#85C8DD'} />
         <spotLight
-          intensity={2}
+          intensity={6}
           angle={0.1}
           position={[20, 30, 30]}
           penumbra={1}
           castShadow
           decay={2}
           power={15}
-          color={'#D5BC76'}
+          color={'#E3C87E'}
         />
         <Sparkles count={200} scale={10} size={2} speed={0.4} opacity={0.01} />
         <InstancedModel />
         <Controls />
-        {/* <EffectComposer>
-          <DepthOfField target={[0, 0, 1]} focalLength={0.2} bokehScale={3} />
+        <EffectComposer>
+          <DepthOfField target={[0, 0, 1]} focalLength={0.007} bokehScale={4} />
           <Bloom
-            kernelSize={4}
-            luminanceThreshold={0.25}
-            luminanceSmoothing={7}
-            intensity={1}
+            kernelSize={6}
+            luminanceThreshold={0}
+            luminanceSmoothing={10}
+            height={100}
+            intensity={100}
+            opacity={0.1}
           />
+          <Noise opacity={0.025} />
           <Vignette eskil={false} blendFunction={BlendFunction.NORMAL} />
-        </EffectComposer> */}
+        </EffectComposer>
       </SheetProvider>
     </Canvas>
   );
